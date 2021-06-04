091e7c68a8d3486d05a1af278db84473ab1975d5
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-06-04 10:42:39 -0500

    Estructura inicial

---
+++ qrreader/lib/pages/map_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MapPageScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('MapaPage'),
+      ),
+    );
+  }
+}
