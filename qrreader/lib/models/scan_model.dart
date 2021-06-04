fab29e5819b39073f355fe86c2b92eab71936b67
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-06-04 15:26:55 -0500

    Se agrega modelo para trabajar con la respuesta del QR

---
+++ qrreader/lib/models/scan_model.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+ScanModel scanModelFromJson(String str) => ScanModel.fromJson(json.decode(str));
+
+String scanModelToJson(ScanModel data) => json.encode(data.toJson());
+
+class ScanModel {
+  ScanModel({
+    this.id = 0,
+    this.tipo = '',
+    required this.valor,
+  }) {
+    if (this.tipo.contains('http')) {
+      this.tipo = 'http';
+    } else {
+      this.tipo = 'geo';
+    }
+  }
+
+  int id;
+  String tipo;
+  String valor;
+
+  factory ScanModel.fromJson(Map<String, dynamic> json) => ScanModel(
+        //reotna una instancia de la clase
+        id: json["id"],
+        tipo: json["tipo"],
+        valor: json["valor"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        //retorna un mapa
+        "id": id,
+        "tipo": tipo,
+        "valor": valor,
+      };
+}
--- qrreader/lib/widgets/scan_custom.dart
+++ qrreader/lib/widgets/scan_custom.dart
@@ -8,8 +8,9 @@ class FloatingActionCustom extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       onPressed: () async {
-        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-            '#3D8BEF', 'Cancelar', false, ScanMode.DEFAULT);
+        //String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#3D8BEF', 'Cancelar', false, ScanMode.DEFAULT);
+
+        String barcodeScanRes = 'https://eltiempo.com';
 
         print(barcodeScanRes);
       },
