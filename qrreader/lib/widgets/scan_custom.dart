aa14ed64dc04027496b39d865ebcfc80d14c8717
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-06-04 15:01:48 -0500

    Se realiza la impl,ementación para leer codigos QR

--- qrreader/lib/widgets/scan_custom.dart
+++ qrreader/lib/widgets/scan_custom.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class FloatingActionCustom extends StatelessWidget {
   const FloatingActionCustom({Key? key}) : super(key: key);
@@ -6,7 +7,12 @@ class FloatingActionCustom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: () {},
+      onPressed: () async {
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+            '#3D8BEF', 'Cancelar', false, ScanMode.DEFAULT);
+
+        print(barcodeScanRes);
+      },
       child: Icon(
         Icons.filter_center_focus,
         color: Colors.black,
