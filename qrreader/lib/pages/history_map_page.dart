306c95ee1456a3f6415e0aff4494a66d95aafbfb
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-06-05 23:04:33 -0500

    Implementacion de Google Maps para Android y IOS

--- qrreader/lib/pages/history_map_page.dart
+++ qrreader/lib/pages/history_map_page.dart
@@ -1,9 +1,25 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:qrreader/providers/db_provider.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HistoryMapPage extends StatelessWidget {
+class HistoryMapPage extends StatefulWidget {
+  @override
+  _HistoryMapPageState createState() => _HistoryMapPageState();
+}
+
+class _HistoryMapPageState extends State<HistoryMapPage> {
+  Completer<GoogleMapController> _controller =
+      Completer(); //Parecido a un future
+
   @override
   Widget build(BuildContext context) {
+    final CameraPosition _kGooglePlex = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962),
+      zoom: 14.4746,
+    );
+
     final ScanModel scan = ModalRoute.of(context)!.settings.arguments
         as ScanModel; //Recupera el valor enviado por el PushName. SE DEBE CASTERA PARA EVITA UN ERROR.
 
@@ -11,8 +27,12 @@ class HistoryMapPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Mapa'),
       ),
-      body: Center(
-        child: Text('Historial de mapas : ${scan.valor}'),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
